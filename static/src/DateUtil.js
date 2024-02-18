76e3c4381cf9ec249ff617ef9f2e3896e3df27fc
landshark <carlsim66@gmail.com>
2024-02-18 15:06:35 -0800

    auto set default date to today

---
+++ static/src/DateUtil.js
@@ -0,0 +1,10 @@
+function getLocalDate(date) {
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+
+    if (month < 10) month = "0" + month;
+    if (day < 10) day = "0" + day;
+
+    return year + "-" + month + "-" + day;
+}
